a57ec0f92d494937b3e8f166033fd12070a54c8c
Arindam-Thinkpad-Laptop <arindamwork7@gmail.com>
2022-10-20 05:54:14 +0530

    added card and order details page

--- mern-ecommerce-food-site-client/src/App.js
+++ mern-ecommerce-food-site-client/src/App.js
@@ -5,14 +5,17 @@ import Pizza from './pages/pizza/Pizza'
 import Pasta from './pages/pasta/Pasta'
 import Burger from './pages/burger/Burger'
 import Beverage from './pages/beverage/Beverage'
+
 import Payment from './pages/payment/Payment';
+import Cart from './pages/cart/Cart';
+import OrderDetails from './pages/orderDetails/OrderDetails';
 
 function App() {
   return (
     <>
 
       <Routes >
-        <Route path='/' element={< Payment />} />
+        <Route path='/' element={< OrderDetails />} />
         {/* <Route path='/pizza' element={< Pizza />} />
         <Route path='/pasta' element={< Pasta />} />
         <Route path='/burger' element={< Burger />} />
