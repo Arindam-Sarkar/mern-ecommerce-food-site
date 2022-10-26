12c7d355f8a75cd49d39a9aa94b09cbecf017894
Arindam-Thinkpad-Laptop <arindamwork7@gmail.com>
2022-10-26 06:51:11 +0530

    Changes in product Zoomed for other items quantity

--- mern-ecommerce-food-site-client/src/components/productZoomed/ProductZoomed.jsx
+++ mern-ecommerce-food-site-client/src/components/productZoomed/ProductZoomed.jsx
@@ -388,11 +388,14 @@ const ProductZoomed = ({ showProductZoomHandler, inputItem }) => {
               <div className='pZExtraQuantityCont'>
                 <label htmlFor="quantity">Choose Quantity</label>
                 <select
+                  value={item.quantity}
                   onChange={(e) => ItemHandler(item.type, "quantity", e.target.value)}
                   name="quantity" id="carquantitys">
-                  <option value="1">1</option><option value="2">2</option>
-                  <option value="3">3</option><option value="4">4</option>
-                  <option value="4">5</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
                 </select>
               </div>
             </div>
